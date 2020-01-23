8d53869a8f6290909b338259b4996dd568a6fa58
nijynot <nijynot@gmail.com>
2020-01-23 17:09:49 +0100

    Eject and make it buildable with `yarn bundle`

--- src/web/webpack.config.js
+++ src/web/webpack.config.js
@@ -5,7 +5,7 @@ const glob = require('glob');
 module.exports = {
   mode: 'production',
   entry: {
-    'bundle.js': glob.sync('build/static/?(js|css)/*.?(js|css)').map(f => path.resolve(__dirname, f)),
+    'bundle.js': './src/index.js',
   },
   output: {
     filename: 'build/static/js/bundle.min.js',
@@ -15,6 +15,37 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: require.resolve('babel-loader'),
+        options: {
+          customize: require.resolve(
+            'babel-preset-react-app/webpack-overrides'
+          ),
+
+          plugins: [
+            [
+              require.resolve('babel-plugin-named-asset-import'),
+              {
+                loaderMap: {
+                  svg: {
+                    ReactComponent:
+                      '@svgr/webpack?-svgo,+titleProp,+ref![path]',
+                  },
+                },
+              },
+            ],
+          ],
+          // This is a feature of `babel-loader` for webpack (not Babel itself).
+          // It enables caching results in ./node_modules/.cache/babel-loader/
+          // directory for faster rebuilds.
+          cacheDirectory: true,
+          // See #6846 for context on why cacheCompression is disabled
+          cacheCompression: false,
+          compact: true,
+        },
       },
     ],
   },
